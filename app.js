e6b6a275884c0f3928973f81df79549be8d36dd4
JaejinHwang <hjj4756@kaist.ac.kr>
2021-07-08 14:52:57 +0900

    02. Login
    implement simple login feature with localStorage

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert(5+2);
+const submitForm = document.querySelector("#login-form")
+const nameInput = document.querySelector("#login-form input");
+const greetMessage = document.querySelector("#greeting")
+
+function handleNameSubmit (event) {
+    event.preventDefault()
+    submitForm.classList.add("hidden");
+    greetMessage.innerText = `hello, ${nameInput.value}`;
+    greetMessage.classList.remove("hidden");
+    localStorage.setItem("userName", nameInput.value);
+}
+
+
+const savedName = localStorage.getItem("userName");
+if (savedName === null) {
+    submitForm.classList.remove("hidden");
+    submitForm.addEventListener("submit", handleNameSubmit);
+} else {
+    greetMessage.innerText = `hello, ${savedName}`;
+    greetMessage.classList.remove("hidden");
+}
